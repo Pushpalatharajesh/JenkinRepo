86f237c16661a19cb89f0382a9885fa9a9b2f2c1
Pushpalatharajesh <pushpahebbar2@gmail.com>
2023-07-12 14:43:09 +0530

    first commit

---
+++ jenkinsPractice/src/test/java/PracticeGitnJenkinProgram/Demo1.java
@@ -0,0 +1,11 @@
+package PracticeGitnJenkinProgram;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+public void m1()
+{
+	System.out.println("Hi Pushpa");
+}
+}
---
+++ jenkinsPractice/src/test/java/PracticeGitnJenkinProgram/Demo2.java
@@ -0,0 +1,13 @@
+package PracticeGitnJenkinProgram;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	
+	public void m2()
+	{
+		System.out.println("Hello ");
+	}
+
+}
